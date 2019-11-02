286cc7f00441fc70e3c8fc8901140e2b575707a6
Garrett <gsilver@buffalo.edu>
2019-11-02 17:46:00 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/geostats/MainActivity.java
+++ app/src/main/java/com/example/geostats/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.geostats;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchBar;
+    private Button mSendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchBar = findViewById(R.id.search_bar_edit_text);
+        mSendButton = findViewById(R.id.search_btn);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mSearchBar.getText().toString().isEmpty()) {
+                    Coordinates coordinates = new Coordinates();
+                    coordinates.setLocation(mSearchBar.getText().toString());
+                }
+                else {
+                    Toast.makeText(
+                            view.getContext(),
+                            "Please input a location",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
